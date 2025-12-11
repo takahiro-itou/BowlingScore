c37dd174e96c0276111fbeac117815310de39d44
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-12-11 20:07:51 +0900

    ソリューションの構成を変更
    (1) C++/CLI を使っているので AnyCPU にはできない

--- Rebuild.All.bat
+++ Rebuild.All.bat
@@ -1,49 +1,33 @@
 
 msbuild -restore -t:Rebuild     ^
-    -p:Configuration="Release"  -p:Platform=x64         ^
+    -p:Configuration="Release"  -p:Platform=x64     ^
     BowlingScoreView.sln
 
 msbuild -restore -t:Rebuild     ^
-    -p:Configuration="Debug"    -p:Platform=x64         ^
+    -p:Configuration="Debug"    -p:Platform=x64     ^
     BowlingScoreView.sln
 
 msbuild -restore -t:Rebuild     ^
-    -p:Configuration="Release"  -p:Platform=x86         ^
+    -p:Configuration="Release"  -p:Platform=x86     ^
     BowlingScoreView.sln
 
 msbuild -restore -t:Rebuild     ^
-    -p:Configuration="Debug"    -p:Platform=x86         ^
-    BowlingScoreView.sln
-
-msbuild -restore -t:Rebuild     ^
-    -p:Configuration="Release"  -p:Platform="Any CPU"   ^
-    BowlingScoreView.sln
-
-msbuild -restore -t:Rebuild     ^
-    -p:Configuration="Debug"    -p:Platform="Any CPU"   ^
+    -p:Configuration="Debug"    -p:Platform=x86     ^
     BowlingScoreView.sln
 
 
 msbuild -restore -t:Rebuild     ^
-    -p:Configuration="Release"  -p:Platform=x64         ^
+    -p:Configuration="Release"  -p:Platform=x64     ^
     BowlingScoreView.NetOld.sln
 
 msbuild -restore -t:Rebuild     ^
-    -p:Configuration="Debug"    -p:Platform=x64         ^
+    -p:Configuration="Debug"    -p:Platform=x64     ^
     BowlingScoreView.NetOld.sln
 
 msbuild -restore -t:Rebuild     ^
-    -p:Configuration="Release"  -p:Platform=x86         ^
+    -p:Configuration="Release"  -p:Platform=x86     ^
     BowlingScoreView.NetOld.sln
 
 msbuild -restore -t:Rebuild     ^
-    -p:Configuration="Debug"    -p:Platform=x86         ^
-    BowlingScoreView.NetOld.sln
-
-msbuild -restore -t:Rebuild     ^
-    -p:Configuration="Release"  -p:Platform="Any CPU"   ^
-    BowlingScoreView.NetOld.sln
-
-msbuild -restore -t:Rebuild     ^
-    -p:Configuration="Debug"    -p:Platform="Any CPU"   ^
+    -p:Configuration="Debug"    -p:Platform=x86     ^
     BowlingScoreView.NetOld.sln
