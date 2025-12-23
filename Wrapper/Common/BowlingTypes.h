d520d12c46a2a5ab3d90c9aff6c29601994fb682
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-12-23 12:26:43 +0900

    空のヘッダファイルを新規作成

---
+++ Wrapper/Common/BowlingTypes.h
@@ -0,0 +1,28 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---   Bowling Score View App   ---                  **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      Type Definitions.
+**
+**      @file       BowlingTypes.h
+**/
+
+#pragma     once
+
+#include    "BowlingScore/Common/BowlingTypes.h"
+
+
+namespace  BsWrap  {
+
+}   //  End of namespace  BsWrap
